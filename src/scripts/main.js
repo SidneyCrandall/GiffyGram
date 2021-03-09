133f8451d2e563e3c94820f4cf1f94057db8303c
Sidney Crandall <sdncrandall5@gmail.com>
2021-03-08 20:24:32 -0600

    Got footer and logout to render. Working on alert messages.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,47 @@
-import { getUsers, getPosts} from "./data/DataManager.js"
-import { PostList } from "./feed/PostList.js"
-import {NavBar} from "./nav/NavBar.js"
+import { getUsers, getPosts } from "./data/DataManager.js";
+import { PostList } from "./feed/PostList.js";
+import { NavBar } from "./nav/NavBar.js";
+import { Footer } from "./nav/Footer.js";
+
 
 
 const showPostList = () => {
-  const postElement = document.querySelector(".postList");
-	getPosts().then((allPosts) => {
-		postElement.innerHTML = PostList(allPosts);
-	})
+    const postElement = document.querySelector(".postList");
+    getPosts().then((allPosts) => {
+        postElement.innerHTML = PostList(allPosts);
+    })
 }
 
 const showNavBar = () => {
-	const navElement = document.querySelector("nav")
+    const navElement = document.querySelector("nav")
     navElement.innerHTML = NavBar();
 }
 
+const applicationElement = document.querySelector("main");
+applicationElement.addEventListener("click", event => {
+    if (event.target.id === "logout") {
+        console.log("You clicked on logout")
+    }
+})
+
+const showFooter = () => {
+    const footerElement = document.querySelector("footer");
+    footerElement.innerHTML = Footer();
+}
+
+applicationElement.addEventListener("change", event => {
+    if (event.target.id === "yearSelection") {
+        const yearAsNumber = parseInt(event.target.value)
+
+        console.log(`User wants to see posts since ${yearAsNumber}`)
+    }
+})
+
 const startGiffyGram = () => {
-	showPostList();
-	showNavBar();
+    showPostList();
+    showNavBar();
+    showFooter();
 }
 
 
 startGiffyGram();
-
-
