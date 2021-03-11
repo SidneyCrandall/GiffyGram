d95157e465933d9c405f1125cb4df3822c7ebd34
Sidney Crandall <sdncrandall5@gmail.com>
2021-03-11 11:12:29 -0600

    enabled creating post, reserving the order.

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,29 +1,40 @@
 export const getUsers = () => {
-
     return fetch("http://localhost:8088/users")
-    .then(response => response.json())
-    .then(parsedResponse => {
-        // do something with response here
-        return parsedResponse;
-    })
+        .then(response => response.json())
+};
+
+let postCollection = [];
+
+export const usePostCollection = () => {
+    return [...postCollection];
 }
 
 export const getPosts = () => {
-
     return fetch("http://localhost:8088/posts")
-    .then(response => response.json())
-    .then(parsedResponse => {
-        // do something with response here
-        return parsedResponse;
-    })
+        .then(response => response.json())
+        .then(parsedResponse => {
+            postCollection = parsedResponse
+            return parsedResponse;
+        })
+}
+
+export const createPost = postObj => {
+    return fetch("http://localhost:8088/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postObj)
+        })
+        .then(response => response.json())
 }
 
 const loggedInUser = {
-	id: 1,
-	name: "Bryan",
-	email: "bryan@bn.com"
-}
+    id: 1,
+    name: "Sidney",
+    email: "Sidney@sc.com"
+};
 
 export const getLoggedInUser = () => {
-	return loggedInUser;
+    return {...loggedInUser };
 }
